eba75051d4a56bf7713e2eba169c846c510a84d3
devan74 <raghavadeva1903@gmail.com>
2023-11-25 13:59:23 +0530

    cors

--- index.js
+++ index.js
@@ -1,58 +1,65 @@
-require("dotenv").config();
-const express = require("express");
-const db = require("./db/connection");
-const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
-//Importing routes
-const authRoutes = require("./routes/auth.routes"); //custom middleware
-// const chatRoutes = require('./routes/chat.routes');
-const userRoutes = require("./routes/user.routes");
+require('dotenv').config();
+const express = require('express');
+const db = require('./db/connection');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors("https://classy-treacle-332e43.netlify.app"));
-
-app.use("/api", authRoutes);
-app.use("/api", userRoutes);
-
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "https://classy-treacle-332e43.netlify.app",
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server,{
+    cors: {
+        origin: 'https://classy-treacle-332e43.netlify.app',
+        methods: ['GET', 'POST']
+    }
+})
+
+//Importing routes
+const authRoutes = require('./routes/auth.routes'); //custom middleware
+// const chatRoutes = require('./routes/chat.routes');
+const userRoutes = require('./routes/user.routes');
 
 //Connecting DB.
 db();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: 'https://classy-treacle-332e43.netlify.app',
+    credentials: true,
+}));
+
+
+app.use('/api',authRoutes);
+app.use('/api', userRoutes);
+
 // on -> receiving side.
 // emit -> sending side.
 
-io.on("connection", (socket) => {
-  socket.on("join-room", (data) => {
-    socket.join(data);
-    console.log(`user ${socket.id} has joined the room ${data}`);
-  });
+io.on('connection', (socket) => {
 
-  socket.on("send-message", (data) => {
-    console.log("Data: ", data);
-    socket.to(data.room).emit("receive-message", data);
-  });
+    socket.on('join-room', (data) => {
+        socket.join(data);
+        console.log(`user ${socket.id} has joined the room ${data}`);
+    });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected: ", socket.id);
-  });
-});
+    socket.on('send-message', (data) => {
+        console.log('Data: ', data );
+        socket.to(data.room).emit('receive-message', data);
+    } );
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected: ', socket.id);
+    })
+})
+
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`App is running on PORT ${PORT}`);
-});
+    console.log(`App is running on PORT ${PORT}`);
+})
+
